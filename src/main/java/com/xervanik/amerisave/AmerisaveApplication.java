28d9b0e96b3ad137ce8679d49ca9f72ee76f7187
John Pettit <john.pettit@cox.net>
2020-12-07 11:09:19 -0800

    Initial commit

---
+++ src/main/java/com/xervanik/amerisave/AmerisaveApplication.java
@@ -0,0 +1,13 @@
+package com.xervanik.amerisave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmerisaveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AmerisaveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xervanik/amerisave/AmerisaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xervanik.amerisave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmerisaveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
